f763923e79d7b5f44c51015753427e4b17130a68
Aquiles <wesley419@gmail.com>
2023-06-29 03:23:21 +0000

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { RechargeCronService } from './utils/recharge-cron.service';
-import * as moment from 'moment-timezone';
-
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const stationsService = app.get(RechargeCronService);
-
-  moment.tz.setDefault('America/Sao_Paulo');
-
 
   // Iniciar o cron job
   stationsService.startCronJob();
